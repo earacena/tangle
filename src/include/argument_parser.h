440de609fed760449bf74d0577be0a0002f00763
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-06-17 10:51:52 -0400

    refactor: add a journal reference and PrintUsage()

--- src/include/argument_parser.h
+++ src/include/argument_parser.h
@@ -1,19 +1,22 @@
 #pragma once
 
-#include "journal.h"
+#include <format>
 #include <functional>
+#include <iomanip>
 #include <map>
 #include <optional>
 #include <vector>
 
+#include "journal.h"
+
 struct Command {
-  std::string short_name;        // used with single dash,  for example "-a"
-  std::string long_name;         // used with double dash, for example "--apply"
-  std::string short_description; // for usage
-  std::string long_description;  // for usage
-  int num_of_args;               // how many expected arguments
-  std::function<void(std::vector<std::string>)>
-      apply_fn; // function that is called to handle command
+  std::string short_name;  // used with single dash,  for example "-a"
+  std::string long_name;   // used with double dash, for example "--apply"
+  std::string short_description;  // for usage
+  std::string long_description;   // for usage
+  int num_of_args;                // how many expected arguments
+  std::function<void(Journal &journal, std::vector<std::string>)>
+      apply_fn;  // function that is called to handle command
 };
 
 struct CommandPair {
@@ -22,12 +25,13 @@ struct CommandPair {
 };
 
 class ArgumentParser {
-public:
+ public:
   ArgumentParser(std::map<std::string, Command> command_table)
       : command_table(std::move(command_table)) {}
 
   auto Parse(int argc, char **argv) -> std::optional<std::vector<CommandPair>>;
+  auto PrintUsage() -> void;
 
-private:
+ private:
   std::map<std::string, Command> command_table;
 };
