ef56eecbad2e09345f434ffeffd4aa721a1c2717
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-06-17 10:53:57 -0400

    feat: add journal constructor

--- src/include/journal.h
+++ src/include/journal.h
@@ -1,27 +1,28 @@
 #pragma once
 
+#include <sodium.h>
+#include <time.h>
+
 #include <chrono>
 #include <ctime>
 #include <exception>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <sodium.h>
 #include <sstream>
 #include <stdexcept>
 #include <string>
-#include <time.h>
 #include <vector>
 
 typedef std::string JournalId;
 
 const uint16_t JOURNAL_ENTRY_SIZE =
-    533; // 4 bytes for id, 4 bytes for size, 25 bytes for date string, 500
-         // bytes for padded content
-const uint16_t CONTENT_MAX_SIZE = 500; // 500 bytes
+    533;  // 4 bytes for id, 4 bytes for size, 25 bytes for date string, 500
+          // bytes for padded content
+const uint16_t CONTENT_MAX_SIZE = 500;  // 500 bytes
 
 class JournalEntry {
-public:
+ public:
   JournalEntry() {
     // Timestamp new entry
     auto now = std::chrono::system_clock::now();
@@ -37,13 +38,12 @@ public:
     auto now = std::chrono::system_clock::now();
     timestamp = std::chrono::system_clock::to_time_t(now);
   };
-
   auto Store(std::fstream &fs) -> void;
   auto Load(std::fstream &fs) -> void;
 
-  int id;                // 4 bytes
-  std::time_t timestamp; // 19 bytes, uses ISO 8601 date string
-  std::string content;   // 500 bytes
+  int id;                 // 4 bytes
+  std::time_t timestamp;  // 19 bytes, uses ISO 8601 date string
+  std::string content;    // 500 bytes
 };
 
 struct JournalState {
@@ -51,25 +51,25 @@ struct JournalState {
 };
 
 class DiskManager {
-public:
+ public:
   DiskManager(const std::string &filename);
-  ~DiskManager();
   auto WriteEntry(JournalEntry &entry) -> void;
   auto ReadEntries() -> std::vector<JournalEntry>;
 
-private:
+ private:
   // Journal file
   std::string journal_name_;
   std::fstream journal_io_;
 };
 
 class Journal {
-public:
+ public:
+  Journal(DiskManager &dm) : dm_(std::move(dm)){};
   auto CreateEntry(std::string &content) -> bool;
 
   std::vector<JournalEntry> entries;
 
-private:
+ private:
   DiskManager dm_;
   int num_of_entries_ = 0;
 };
