aba4a21e6eb34aeca4952f0093d3d34cb6233f84
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-06-27 19:07:35 -0400

    test: add test for GetEnvVarString()

--- tests/util_test.cpp
+++ tests/util_test.cpp
@@ -3,11 +3,20 @@
 #include <catch2/catch_test_macros.hpp>
 #include <string>
 
-TEST_CASE("Correctly parses seconds from epoch (UNIX timestamp) into string",
-          "[date]") {
+TEST_CASE(
+    "SecondsToDateString(), Correctly parses seconds from epoch (UNIX "
+    "timestamp) into string",
+    "[date]") {
   std::chrono::system_clock::rep seconds = 1719246515;
 
   auto date_string = SecondsToDateString(seconds);
 
   REQUIRE(date_string.compare("2024-06-24 12:28:35") == 0);
+}
+
+TEST_CASE("GetEnvVarString(), Correctly fetches an evironment variable",
+          "[env]") {
+  auto value = GetEnvVarString("HOME");
+
+  REQUIRE(value.size() != 0);
 }
